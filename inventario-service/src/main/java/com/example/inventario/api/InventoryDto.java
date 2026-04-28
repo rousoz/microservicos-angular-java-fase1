4013caa52006582524598cf2dc0806391aaa0330
rousoz <rousoz@yahoo.com.br>
2026-04-28 09:21:50 -0300

    espaçamento

--- inventario-service/src/main/java/com/example/inventario/api/InventoryDto.java
+++ inventario-service/src/main/java/com/example/inventario/api/InventoryDto.java
@@ -2,10 +2,6 @@ package com.example.inventario.api;
 
 public class InventoryDto {
 
-    //public record InventoryRequest(Object object, Object object2) {
-    //        //TODO Auto-generated constructor stub
-    //}
-
     public record InventoryRequest(Long productId, Integer quantity) {
     }
 
